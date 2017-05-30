3fe70959666fe34cabfc1982f03833ad20be77db
Roy Henley <Roy.Henley@sportsbet.com.au>
2017-05-30 12:03:42 +1000

    demo

---
+++ src/main/java/com/example/avro/Application.java
@@ -0,0 +1,72 @@
+package com.example.avro;
+
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by royh on 30/05/2017.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        Application app = new Application();
+        try {
+            File file = new File("users.avro");
+            app.serialise(file);
+            app.deserialise(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void serialise (File file) throws IOException {
+
+        // use default favourite colour
+        User user1 = new User();
+        user1.setName("Bob");
+        user1.setFavoriteNumber(256);
+
+        // Alternate constructor
+        User user2 = new User("Ben", 7, "red");
+
+        // Construct via builder
+        User user3 = User.newBuilder()
+                .setName("Charlie")
+                .setFavoriteColor("blue")
+                .setFavoriteNumber(null)
+                .build();
+
+        // Serialize user1, user2 and user3 to disk
+        DatumWriter<User> userDatumWriter = new SpecificDatumWriter<>(User.class);
+        DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
+        dataFileWriter.create(user1.getSchema(), file);
+        dataFileWriter.append(user1);
+        dataFileWriter.append(user2);
+        dataFileWriter.append(user3);
+        dataFileWriter.close();
+    }
+
+    public void deserialise (File file) throws IOException {
+
+        // Deserialize Users from disk
+        DatumReader<User> userDatumReader = new SpecificDatumReader<>(User.class);
+        DataFileReader<User> dataFileReader = new DataFileReader<User>(file, userDatumReader);
+        User user = null;
+
+        while (dataFileReader.hasNext()) {
+            // Reuse user object by passing it to next(). This saves us from
+            // allocating and garbage collecting many objects for files with
+            // many items.
+            user = dataFileReader.next(user);
+            System.out.println(user);
+        }
+    }
+
+}
